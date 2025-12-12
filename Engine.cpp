cd14328cc7f1cb2f782884e495402cb5c243854b
Your Name <you@example.com>
2025-12-11 16:53:10 -0800

    Completed Project

---
+++ Engine.cpp
@@ -0,0 +1,158 @@
+#include "Engine.h"
+
+
+
+
+Engine::Engine(){ 
+  
+   m_Window.create(VideoMode::getDesktopMode(), "Particles");
+
+    if(!m_Music.openFromFile("1979.ogg")){
+
+        cerr << "Error: Could not file '1979.mp3";
+    }
+    else {
+
+        m_Music.setLoop(true);
+        m_Music.play();
+    }
+
+}
+
+
+
+
+void Engine::run(){
+
+
+   Clock clock;
+
+
+   cout << "Starting Particle unit tests..." << endl;
+   Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+   p.unitTests();
+   cout << "Unit tests complete.  Starting engine..." << endl;
+
+
+   while(m_Window.isOpen()){
+
+
+       Time dt = clock.restart();
+       float dtAsSeconds = dt.asSeconds();
+
+
+       input();
+
+
+       update(dtAsSeconds);
+
+
+       draw();
+   }
+
+
+
+
+}
+
+
+
+
+void Engine::input(){
+
+
+   Event event;
+
+
+   while(m_Window.pollEvent(event)){
+
+
+       if (event.type == Event::Closed || event.type == Event::KeyPressed && event.key.code == Keyboard::Escape){
+
+
+           m_Window.close();
+       }
+
+
+       if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left){
+
+
+           Vector2i mouseClickPosition = {event.mouseButton.x, event.mouseButton.y};
+
+
+           int numParticles = 5;
+
+
+           for (int i = 0; i < numParticles; i++){
+
+
+               int numPoints = (rand() % (50 - 25 + 1)) + 25;
+
+
+               Particle newParticle(m_Window, numPoints, mouseClickPosition);
+
+
+               m_particles.push_back(newParticle);
+           }
+       }
+   }
+
+
+
+
+
+
+}
+
+
+
+
+void Engine::update(float dtAsSeconds){
+
+
+   for (int i = 0; i < m_particles.size();){
+
+
+       if (m_particles[i].getTTL() > 0.0){
+
+
+           m_particles[i].update(dtAsSeconds);
+          
+           i++;
+       }
+       else {
+
+
+           m_particles.erase(m_particles.begin() + i);
+       }
+   }
+
+
+
+
+}
+
+
+
+
+void Engine::draw(){
+
+
+   m_Window.clear(sf::Color(49, 81, 107));
+
+
+   for (int i = 0; i < m_particles.size(); i++){
+
+
+       m_Window.draw(m_particles[i]);
+   }
+
+
+   m_Window.display();
+
+
+}
+
+
+
+
---
+++ Matrices.cpp
@@ -0,0 +1,140 @@
+#include "Matrices.h"
+#include <iomanip>
+
+
+namespace Matrices
+{
+
+    RotationMatrix::RotationMatrix(double theta) : Matrix(2,2) {
+
+
+        (*this)(0, 0) = cos(theta);
+        (*this)(0, 1) = -sin(theta);
+        (*this)(1, 0) = sin(theta);
+        (*this)(1, 1) = cos(theta);
+
+
+}
+
+
+    ScalingMatrix::ScalingMatrix(double scale) : Matrix(2,2) {
+
+
+        double c = scale;
+
+
+        (*this)(0, 0) = c;
+        (*this)(0, 1) = 0;
+        (*this)(1, 0) = 0;
+        (*this)(1, 1) = c;
+
+
+}
+
+
+    TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2, nCols) {
+  
+        for (int i = 0; i < nCols; i++){
+
+
+        (*this)(0, i) = xShift;
+        (*this)(1, i) = yShift;
+
+
+   }
+}
+
+	Matrix::Matrix(int _rows, int _cols) : rows(_rows), cols(_cols), a(_rows, vector<double>(_cols, 0.0)) 
+    { 
+
+    }
+
+	Matrix operator+(const Matrix& a, const Matrix& b) {
+		Matrix result(a.getRows(), a.getCols());
+
+		if (a.getRows() != b.getRows() || a.getCols() != b.getCols()) {
+			throw runtime_error("Error: dimensions must agree");
+		}
+
+		for (int i = 0; i < a.getRows(); i++) {
+			for (int j = 0; j < a.getCols(); j++) {
+				result(i, j) = a(i, j) + b(i, j);
+			}
+		}
+		return result;
+	}
+
+	Matrix operator*(const Matrix& a, const Matrix& b) {
+		Matrix result(a.getRows(), b.getCols());
+
+		if (a.getCols() != b.getRows()) {
+			throw runtime_error("Error: dimensions must agree");
+		}
+
+		for (int k = 0; k < b.getCols(); k++) {
+			for (int i = 0; i < a.getRows(); i++) {
+				double sum = 0.0;
+
+				for (int j = 0; j < a.getCols(); j++) {
+					sum += a(i, j) * b(j, k);
+				}
+				result(i, k) = sum;
+			}
+		}
+		
+		return result;
+	}
+
+	bool operator==(const Matrix& a, const Matrix& b) {
+		
+		if (a.getRows() != b.getRows() || a.getCols() != b.getCols()) {
+			return false;
+		}
+
+		for (int i = 0; i < a.getRows(); i++) {
+			for (int j = 0; j < a.getCols(); j++) {
+				if (abs(a(i, j) - b(i, j)) >= 0.001) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	bool operator!=(const Matrix& a, const Matrix& b) {
+		if (a.getRows() != b.getRows() || a.getCols() != b.getCols()) {
+			return true;
+		}
+
+		for (int i = 0; i < a.getRows(); i++) {
+			for (int j = 0; j < a.getCols(); j++) {
+				if (abs(a(i, j) - b(i, j)) >= 0.001) {
+					return true;
+				}
+			}
+		}
+		return false;
+		
+	}
+
+	ostream& operator<<(ostream& os, const Matrix& a) {
+
+		for (int i = 0; i < a.getRows(); i++) {
+			for (int j = 0; j < a.getCols(); j++) {
+				os << setw(10) << a(i, j);
+
+				if (j < a.getCols() - 1) {
+					os << ' ';
+				}
+			
+			}
+			os << "\n";
+		}
+		return os;
+	}
+
+
+
+
+
+}
